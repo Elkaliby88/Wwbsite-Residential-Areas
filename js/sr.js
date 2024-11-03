f1c3def976b4a7ea07148b68411eee9432fd0dec
Elkaliby88 <148172571+Elkaliby88@users.noreply.github.com>
2024-11-03 16:14:12 +0200

    Add files via upload

---
+++ js/sr.js
@@ -0,0 +1,7 @@
+/*Scroll Reveal*/
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+});
---
+++ js/swiperPopular.js
@@ -0,0 +1,6 @@
+var swiperPopular = new Swiper(".popular__container", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
